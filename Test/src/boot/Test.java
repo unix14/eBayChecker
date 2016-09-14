1b806231e01224a4e0727e935acb62c92fe73fa5
unix14 <unix14@gmail.com>
2016-09-14 19:04:21 +0300

    added I.O Testing code

--- Test/src/boot/Test.java
+++ Test/src/boot/Test.java
@@ -1,12 +1,47 @@
 package boot;
 
-import algorithms.demo.Demo;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import algorithms.io.MyCompressorOutputStream;
+import algorithms.io.MyDecompressorInputStream;
+import algorithms.mazeGenerators.Maze3d;
+import algorithms.mazeGenerators.MazeArgInit;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Demo d1 = new Demo();
-		d1.run();
+		//Demo d1 = new Demo();
+		//d1.run();
+		
+		Maze3d maze = new Maze3d(new MazeArgInit(4, 2, 2));
+		// save it to a file
+		OutputStream out;
+		try {
+			out = new MyCompressorOutputStream(new FileOutputStream("1.maz"));
+			out.write(maze.toByteArray());
+			out.flush();
+			out.close();
+			InputStream in=new MyDecompressorInputStream(new FileInputStream("1.maz"));
+			byte b[]=new byte[maze.toByteArray().length];
+			in.read(b);
+			in.close();
+			Maze3d loaded=new Maze3d(b);
+			System.out.println(loaded.equals(maze));
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
