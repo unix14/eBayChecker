450b781cec727281c019907eaa22971d6672c00b
unix14 <unix14@gmail.com>
2016-09-15 15:14:45 +0300

    edited properties class

--- MVC/src/boot/Run.java
+++ MVC/src/boot/Run.java
@@ -14,8 +14,6 @@ import controller.Command;
 import controller.Controller;
 import controller.Properties;
 import view.CLIView;
-import view.MyGUIView;
-import view.gui.StartWindow;
 
 public class Run {
 	private static Properties properties;
@@ -42,15 +40,10 @@ public class Run {
 		initProperties();
 		
 		HashMap<String, Command> hmap = new HashMap<String, Command>();
-
-		if (properties.getInterfaceView().equals("GUI")) {
-			StartWindow s = new StartWindow(1300, 1000, properties.getSearchAlgo());
 			MyModel m = new MyModel(properties.getNumOfThreads());
-		
-			MyGUIView v = new MyGUIView(hmap, s);
-			Presenter p = new Presenter(m, v);
+			CLIView v = new CLIView(hmap);
+			Controller p = new Controller(m, v);
 
-			s.addObserver(v);
 			v.addObserver(p);
 			m.addObserver(p);
 
@@ -79,8 +72,11 @@ public class Run {
 				@Override
 				public void doCommand(String[] param) {
 					if (param[1].equals("cross"))
-						m.displayCrossSection(param[4], Integer.parseInt(param[5]), param[6]);
-
+						try {
+							m.displayCrossSection(param[4], Integer.parseInt(param[5]), param[6]);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					else if (param[1].equals("solution"))
 						m.displaySolution(param[2]);
 
@@ -94,137 +90,18 @@ public class Run {
 
 				@Override
 				public void doCommand(String[] param) {
-					if (param[1].equals("solution"))
-						try {
+					try{
+						if (param[1].equals("solution"))
 							m.saveSolution(param[2]);
-						} catch (FileNotFoundException e) {
-						} catch (IOException e) {
-						}
-
-					else
-						try {
+						else
 							m.saveMaze(param[2], param[3]);
-						} catch (IOException e) {
-						}
-				}
-			});
-
-			hmap.put("load", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					try {
-						m.loadMaze(param[2], param[3]);
-					} catch (IOException e) {
+					}catch (IOException  e) {
+						e.printStackTrace();
 					}
 
 				}
 			});
 
-			hmap.put("maze", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					m.mazeSize(param[2]);
-
-				}
-			});
-
-			hmap.put("file", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					m.fileSize(param[2]);
-
-				}
-			});
-
-			hmap.put("solve", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					m.solve(param[1], param[2]);
-
-				}
-			});
-
-			hmap.put("exit", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					m.exit();
-
-				}
-			});
-
-			new Thread(s).start();
-		}
-		
-		if(properties.getInterfaceView().equals("CLI")){
-			
-			MyModel m = new MyModel(properties.getNumOfThreads());
-			
-			MyCLIView v = new MyCLIView(hmap);
-			Presenter p = new Presenter(m, v);
-
-			v.addObserver(p);
-			m.addObserver(p);
-
-			/* hash map creates all the commands here */
-			hmap.put("dir", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					m.dir(param[1]);
-				}
-			});
-
-			hmap.put("generate", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					// generate 3d maze
-					m.generate3DMaze(param[3], Integer.parseInt(param[4]), Integer.parseInt(param[5]),
-							Integer.parseInt(param[6]));
-
-				}
-			});
-
-			hmap.put("display", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					if (param[1].equals("cross"))
-						m.displayCrossSection(param[4], Integer.parseInt(param[5]), param[6]);
-
-					else if (param[1].equals("solution"))
-						m.displaySolution(param[2]);
-
-					else
-						m.display(param[1]);
-
-				}
-			});
-
-			hmap.put("save", new Command() {
-
-				@Override
-				public void doCommand(String[] param) {
-					if (param[1].equals("solution"))
-						try {
-							m.saveSolution(param[2]);
-						} catch (FileNotFoundException e) {
-						} catch (IOException e) {
-						}
-
-					else
-						try {
-							m.saveMaze(param[2], param[3]);
-						} catch (IOException e) {
-						}
-				}
-			});
-
 			hmap.put("load", new Command() {
 
 				@Override
@@ -232,6 +109,7 @@ public class Run {
 					try {
 						m.loadMaze(param[2], param[3]);
 					} catch (IOException e) {
+						e.printStackTrace();
 					}
 
 				}
@@ -275,7 +153,6 @@ public class Run {
 
 			new Thread(v).start();
 			
-		}
 
 	}
 }
