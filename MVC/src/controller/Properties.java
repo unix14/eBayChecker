7b60fe0508aec78e199b33bbd70f52a40aaf35a0
unix14 <unix14@gmail.com>
2016-09-15 15:03:30 +0300

    added properties class to controller

---
+++ MVC/src/controller/Properties.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.io.Serializable;
+/**
+ * <b>Properties class </b><br>
+ * holds the properties of the maze.
+ */
+@SuppressWarnings("serial")
+public class Properties implements Serializable {
+	private int numOfThreads;
+
+	
+	
+}
