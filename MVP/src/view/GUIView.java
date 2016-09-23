d252cdb8400aceaf8a0acb6d165aed2aa8e39d00
unix14 <unix14@gmail.com>
2016-09-23 18:21:39 +0300

    GUI improvments

--- MVP/src/view/GUIView.java
+++ MVP/src/view/GUIView.java
@@ -1,5 +1,66 @@
 package view;
 
-public class GUIView {
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
+
+import algorithms.mazeGenerators.Maze3d;
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Solution;
+import presenter.Command;
+import view.gui.StartWindow;
+
+public class GUIView extends ObservableView implements View, Observer  {
+	private StartWindow sw;
+	
+	public GUIView(HashMap<String, Command> hmap, StartWindow sw) {
+		super(hmap); // initializes the hash map in ObservableView
+		this.sw = sw;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		//gets the command from GUI and notify Presenter.
+		line = ((String) arg1).split(" ");
+		command = hmap.get(line[0]);
+		setChanged();
+		notifyObservers(command);
+		
+	}
+
+	@Override
+	public void displayMaze(Maze3d maze) {
+		sw.setMaze(maze);
+	}
+
+	@Override
+	public void displayDir(String[] string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayHint(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayCrossSecion(int[][] maze2d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displaySize(int size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displaySolution(Solution<Position> solution) {
+		sw.setSolution(solution);
+		
+	}
 
 }
